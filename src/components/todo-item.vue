4b6f968b7b985ceb89d4ba554fc5fe147a39f8ca
yvonne.yang <YvongYang@163.com>
2018-11-05 23:04:54 +0800

    Add some events but not all

--- src/components/todo-item.vue
+++ src/components/todo-item.vue
@@ -1,18 +1,41 @@
 <template>
   <div>
-    <input 
-      type="radio"
-    >
+    <div>
+      <input type="radio" class="toggle-complete">
+      <label @dblclick="editTodo(todo)">{{todo.value}}</label>
+      <button class="delete" @click="removeTodo(todo)">X</button>
+    </div>
     <input
+      class="hidden"
       type="text"
-      value="cjzxcj"
-    >
-    <button class="delete">X</button>
+      @blur="doneEdit(todo)"
+      @keyup.enter="doneEdit(todo)"
+      @keyup.esc="cancelEdit(todo)">
   </div>
 </template>
 
 <script>
   export default {
-
+    data() {
+      return {
+        todo: {
+          value: '11'
+        }
+      }
+    },
+    watch: {},
+    computed: {},
+    methods: {
+      editTodo() {},
+      doneEdit() {},
+      cancelEdit() {},
+      removeTodo() {}
+    }
   };
 </script>
+
+<style>
+  .hidden {
+    display: none;
+  }
+</style>
